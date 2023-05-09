6f8f6b0055978d9683258cb76890ab72bc6d0996
Chanison Aupathin <89581992+ChanisonM@users.noreply.github.com>
2023-05-09 21:54:06 +0700

    chanison

--- js/hex.js
+++ js/hex.js
@@ -18,24 +18,4 @@ function getRanNumberArray(){
     return Math.floor(Math.random() * hex.length)
 }
 
-/* แนวคิด
 
-    - สร้าง array เก็บค่า 0 - 9 และ A - F
-
-    - ดึงค่า element ขอ id 'btn' จากไฟล์ hex.html เก็บค่าไว้ตัวแปร const btn
-
-    - ดึงค่า selector ขอ class 'color' จากไฟล์ hex.html เก็บค่าไว้ตัวแปร const color
-
-    - เพิ่มอีเว้นท์ คลิ๊ก พร้อมฟังก์ชั่น เก็บค่าไว้ตัวแปร const btn
-    
-    - สร้างตัวแปร hexColor เก็บค่า # 
-
-    - วนลูป for หลังจากนั้นให้ hexColor += hex[getRanNumberArray()] หมายถึง # แล้วตามด้วย ตัวเลขที่ random มาจาก ฟังก์ชัน getRanNumberArray() อีก 6 ตัว
-
-    - แสดงข้อความ หลัง random hexColor ออกเป็นรูปแบบ Text
-
-    - เซ็ต backgroundColor 
-
-
-*/
- 
